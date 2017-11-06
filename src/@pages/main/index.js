b4f67c1bc5b2efe79783ad49adf06be49374b693
laihuanmin <lai_huanmin@163.com>
2017-11-07 01:05:09 +0800

    add main page

--- src/@pages/main/index.js
+++ src/@pages/main/index.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
-import TopNav from "@snippet/TopNav/index.js";
+import "./index.less";
+import { Link } from "react-router";
+import FengjingImg from "@assert/img/fengjing.jpg";
 
 export default class Current extends Component {
   render() {
     return (
-      <div>
-        <TopNav />
+      <div className="main-container">
+        <div className="image-wrapper">
+          <img src={FengjingImg} />
+        </div>
+        <div className="topNav-wrapper">
+          <div className="container">
+            <div className="column float-left">
+              <h3 className="title">LaiHuanMin的Web前端之旅</h3>
+            </div>
+            <div className="column float-right" style={{ marginLeft: "10px" }}>
+              <Link to="/projectDetail">开源项目</Link>
+            </div>
+            <div className="column float-right" style={{ marginLeft: "10px" }}>
+              <Link to="/projectDetail">个人简历</Link>
+            </div>
+            <div className="column float-right">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/LaiHuanMin"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="content-wrapper container">
+          <div className="content-panel">hello</div>
+          <div className="notice-text">建议在PC端下浏览，效果最佳！</div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Router, hashHistory as history } from "react-router";
 import routes from "@route/index.js";
 import "milligram";
+import "./index.css";
 
 ReactDOM.render(
   <Router routes={routes} history={history}>
